5501533d52cfe5dfd40fe0ac6ec193615dbda257
Youngjo Gil <dudwh921112@naver.com>
2023-07-10 22:57:03 +0900

    feat: 글목록 조회 연동

--- mixins/post-mixin.js
+++ mixins/post-mixin.js
@@ -1,15 +1,46 @@
 export default {
   data() {
     return {
+      // 글 목록
+      posts: [],
+      // 글 상세
       detailPost: {},
+      observer: null,
+      page: -1,
     };
   },
+  mounted() {
+    this.initializeObserver();
+  },
+
   methods: {
     async getDetailPost(id) {
       const result = await this.$api
         .request("get", `/api/admin/post/${id}`)
         .then((res) => (this.detailPost = res.data))
         .catch((error) => console.log(error));
+    },
+    async getPosts() {
+      const result = await this.$api
+        .request("get", `/api/admin/post/list/${this.page}`)
+        .then((res) => this.posts.push(...res.data.content))
+        .catch((error) => console.log(error));
+    },
+    initializeObserver() {
+      const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.7,
+      };
+
+      this.observer = new IntersectionObserver(this.handleIntersect, options);
+      this.observer.observe(this.$refs.scrollTrigger); // 관찰 대상 요소로 등록
+    },
+    handleIntersect(entries) {
+      if (entries[0].isIntersecting) {
+        this.page++;
+        this.getPosts();
+      }
     },
   },
 };
---
+++ pages/post/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>POST</h1>
+
+    <div v-for="(item, index) in posts" :key="index">
+      <div>{{ item.title }}</div>
+    </div>
+
+    <div ref="scrollTrigger"></div>
+  </div>
+</template>
+
+<script>
+import postMixin from "@/mixins/post-mixin";
+export default {
+  mixins: [postMixin],
+  mounted() {},
+  watch: {
+    posts(newVal, oldVal) {
+      console.log(newVal);
+      newVal.map((item) => console.log(item));
+    },
+  },
+};
+</script>
